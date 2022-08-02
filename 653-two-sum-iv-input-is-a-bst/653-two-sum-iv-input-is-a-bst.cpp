3b4ffacd3d34ba760a99562d66446d15747f97fe
Nimit Jain <55830302+Nimit5@users.noreply.github.com>
2022-08-02 09:53:09 +0530

    Time: 72 ms (41.08%), Space: 36.9 MB (62.34%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -9,30 +9,55 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class BSTIterator {
+    stack<TreeNode *> myStack;
+    bool reverse = true; 
+public:
+    BSTIterator(TreeNode *root, bool isReverse) {
+        reverse = isReverse; 
+        pushAll(root);
+    }
+
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        return !myStack.empty();
+    }
+
+    /** @return the next smallest number */
+    int next() {
+        TreeNode *tmpNode = myStack.top();
+        myStack.pop();
+        if(!reverse) pushAll(tmpNode->right);
+        else pushAll(tmpNode->left);
+        return tmpNode->val;
+    }
+
+private:
+    void pushAll(TreeNode *node) {
+        for(;node != NULL; ) {
+             myStack.push(node);
+             if(reverse == true) {
+                 node = node->right; 
+             } else {
+                 node = node->left; 
+             }
+        }
+    }
+};
 class Solution {
 public:
-    void solve(TreeNode* root, int k, map<int,int>&mp, bool &flag)
-    {
-        if(!root)
-            return;
-        if(flag)
-            return;
-        solve(root->left,k,mp,flag);
-        if(mp.find((k-root->val))!=mp.end())
-        {
-            flag=true;
-            return;
-        }
-        else
-        {
-            mp[root->val]++;
-        }
-        solve(root->right,k,mp,flag);
-    }
     bool findTarget(TreeNode* root, int k) {
-        map<int,int>mp;
-        bool flag=false;
-        solve(root,k,mp,flag);
-        return flag;
+        if(!root) return false; 
+        BSTIterator l(root, false); 
+        BSTIterator r(root, true); 
+        
+        int i = l.next();   // next
+        int j = r.next();   // before
+        while(i<j) {
+            if(i + j == k) return true; 
+            else if(i + j < k) i = l.next(); 
+            else j = r.next(); 
+        }
+        return false; 
     }
 };
