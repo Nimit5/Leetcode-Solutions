fcbf02af598c9d1a970980adc8cbac77916ade30
Nimit Jain <55830302+Nimit5@users.noreply.github.com>
2022-08-04 00:31:52 +0530

    Time: 208 ms (78.71%), Space: 34.8 MB (95.57%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    bool checkValid(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        for(int i=0;i<n;i++)
-        {
-            set<int>s;
-            for(int j=0;j<n;j++)
-            {
-                s.insert(matrix[i][j]);
+    bool checkValid(vector<vector<int>>& mat){
+        int n= mat.size();	
+        //mark position by making negative from positive ROW-WISE
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+             int pos= abs(mat[i][j]) -1 ;  // Getting position by value
+
+             if( mat[i][pos] < 0 ) return false;    //If place already marked 
+             mat[i][pos] = -mat[i][pos];    //Mark Place
             }
-            if(s.size()!=n)
-                return false;
         }
-        for(int j=0;j<n;j++)
-        {
-            set<int>s;
-            for(int i=0;i<n;i++)
-            {
-                s.insert(matrix[i][j]);
+
+        //mark position by making negative to positive COLUMN-WISE
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+             int pos= abs(mat[j][i]) -1 ;   // Getting position by value
+
+             if( mat[pos][i] > 0 ) return false;    //If place already marked 
+             mat[pos][i]= abs(mat[pos][i]);        //Mark Place
             }
-            if(s.size()!=n)
-                return false;
         }
+
         return true;
-    }
+}
 };
