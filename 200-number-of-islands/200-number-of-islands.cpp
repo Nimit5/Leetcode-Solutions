393fecef3a7c9e33f3c118d5d80d7dd75165b8ab
Nimit Jain <55830302+Nimit5@users.noreply.github.com>
2022-08-09 23:32:47 +0530

    Time: 50 ms (62.20%), Space: 19.2 MB (8.77%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,27 +1,52 @@
 class Solution {
 public:
+    void bfs(int i, int j, int n, int m, vector<vector<char>>& grid, vector<vector<int>>& vis)
+    {
+        queue<pair<int,int>> q;
+        vis[i][j]=1;
+        q.push({i,j});
+        while(!q.empty())
+        {
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            if(row>0 && grid[row-1][col]=='1' && !vis[row-1][col])
+            {
+                vis[row-1][col]=1;
+                q.push({row-1,col});
+            }
+            if(row<n-1 && grid[row+1][col]=='1' && !vis[row+1][col])
+            {
+                vis[row+1][col]=1;
+                q.push({row+1,col});
+            }
+            if(col>0 && grid[row][col-1]=='1' && !vis[row][col-1])
+            {
+                vis[row][col-1]=1;
+                q.push({row,col-1});
+            }
+            if(col<m-1 &&grid[row][col+1]=='1' && !vis[row][col+1])
+            {
+                vis[row][col+1]=1;
+                q.push({row,col+1});
+            }
+        }
+    }
     int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size(), n = m ? grid[0].size() : 0, islands = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1') {
-                    islands++;
-                    eraseIslands(grid, i, j);
+        int n=grid.size(),m=grid[0].size();
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]=='1' && !vis[i][j])
+                {
+                    count++;
+                    bfs(i,j,n,m,grid,vis);
                 }
             }
         }
-        return islands;
-    }
-private:
-    void eraseIslands(vector<vector<char>>& grid, int i, int j) {
-        int m = grid.size(), n = grid[0].size();
-        if (i < 0 || i == m || j < 0 || j == n || grid[i][j] == '0') {
-            return;
-        }
-        grid[i][j] = '0';
-        eraseIslands(grid, i - 1, j);
-        eraseIslands(grid, i + 1, j);
-        eraseIslands(grid, i, j - 1);
-        eraseIslands(grid, i, j + 1);
+        return count;
     }
 };
