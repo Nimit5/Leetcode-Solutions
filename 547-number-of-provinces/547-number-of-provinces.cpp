0492bef3ec2e177820dc036846fd9111b1a18b00
Nimit Jain <55830302+Nimit5@users.noreply.github.com>
2022-08-09 14:55:14 +0530

    Time: 75 ms (7.34%), Space: 15.1 MB (12.02%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,55 +1,36 @@
 class Solution {
 public:
-    int check(vector<bool> &vis)
-    {
-        for(int i=0;i<vis.size();i++)
-        {
-            if(!vis[i])
-                return i; 
-        }
-        return -1;
-    }
-    void dfs(int node, vector<int> adj[], vector<int> &ans, vector<bool> &vis)
+    // DFS Traversal
+    void dfs(int node, vector<int> adj[], vector<bool> &vis)
     {
         vis[node]=true;
-        ans.push_back(node);
         for(auto it: adj[node])
         {
             if(!vis[it])
-                dfs(it,adj,ans,vis);
-        }
-    }
-    void dfsOfGraph(int V, vector<int> adj[], int &count) {
-        vector<bool> vis(V,false);
-        vector<int> ans;
-        count++;
-        dfs(0,adj,ans,vis);
-        while(true)
-        {
-            int pt=check(vis);
-            if(pt==-1)
-                return;
-            else
-            {
-                count++;
-                dfs(pt,adj,ans,vis);
-            }
+                dfs(it,adj,vis);
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int V=isConnected.size();
         vector<int> adj[V];
+        
+        // Convert Adjacency Matrix to Adjacency List
         for(int i=0;i<V;i++)
-        {
             for(int j=0;j<V;j++)
-            {
-                if(isConnected[i][j]==1)
-          
+                if(isConnected[i][j]==1 && i!=j)
+                {
                     adj[i].push_back(j);
-            }
-        }
+                    adj[j].push_back(i);
+                }    
+        
         int count=0;
-        dfsOfGraph(V,adj,count);
+        vector<bool> vis(V,false);
+        for(int i=0;i<V;i++)
+            if(!vis[i])
+            {
+                count++;
+                dfs(i,adj,vis);
+            }
         return count;
     }
 };
