45ea5af9168e86a48e94f933b54ec6e6bf1d5d2d
Nimit Jain <55830302+Nimit5@users.noreply.github.com>
2022-08-18 00:43:54 +0530

    Time: 1052 ms (50.10%), Space: 178.6 MB (61.93%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -27,9 +27,7 @@ public:
     }
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
         int timer=0;
-        vector<int> tin(n,-1);
-        vector<int> low(n,-1);
-        vector<int> vis(n,0);
+        vector<int> tin(n,-1),low(n,-1),vis(n,0);
         vector<int> adj[n];
         vector<vector<int>> ans;
         for(int i=0;i<connections.size();i++)
@@ -38,10 +36,8 @@ public:
             adj[connections[i][1]].push_back(connections[i][0]);
         }
         for(int i=0;i<n;i++)
-        {
             if(!vis[i])
                 dfs(i,-1,vis,tin,low,timer,adj,ans);
-        }
         return ans;
     }
 };
