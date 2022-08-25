6a26a8486e0545dcddfe45185caf69d4299bbd69
Nimit Jain <55830302+Nimit5@users.noreply.github.com>
2022-08-25 10:40:27 +0530

    Time: 16 ms (80.72%), Space: 8.8 MB (72.69%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        
         int arr[26]={0};
+        //Increase count of alphabet in magazine
         for(int i=0;i<magazine.size();i++)
             arr[magazine[i]-'a']++;
+        
+        //Decrease count of alphabet in ransomNote
         for(int i=0;i<ransomNote.size();i++)
             arr[ransomNote[i]-'a']--;
+        
+        //If count less than zero for any alphabet, it is not possible to construct             ransomNote
         for(int i=0;i<26;i++)
-        {
             if(arr[i]<0)
                 return false;
-        }
+        
         return true;
     }
 };
