d9d4e2aa646346e290c92b7fc4edc26b898b6a5c
Nimit Jain <55830302+Nimit5@users.noreply.github.com>
2022-09-01 15:31:10 +0530

    Time: 9 ms (69.20%), Space: 13.3 MB (25.43%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int m=grid.size(),n=grid[0].size(),countFresh=0;
-        vector<vector<int>> vis(m,vector<int>(n,0));
         queue<pair<pair<int,int>,int>>q;
+        
+        int m=grid.size(),n=grid[0].size(),countfresh=0;
+        vector<vector<int>>vis(m,vector<int>(n,0));
         for(int i=0;i<m;i++)
             for(int j=0;j<n;j++)
-            {    
                 if(grid[i][j]==2)
-                {
-                    q.push({{i,j},0});
-                    vis[i][j]=2;
-                }
+                    q.push({{i,j},0}),vis[i][j]=1;
                 else if(grid[i][j]==1)
-                    countFresh++;
-            }
-        int time=0,cnt=0;
-        int drow[]={-1,0,+1,0};
-        int dcol[]={0,1,0,-1};
+                    countfresh++;
+        int ans=0,cnt=0;
+        int drow[4]={-1,0,0,1};
+        int dcol[4]={0,-1,1,0};
         while(!q.empty())
         {
-            int r=q.front().first.first;
-            int c=q.front().first.second;
+            int row=q.front().first.first;
+            int col=q.front().first.second;
             int t=q.front().second;
-            time=max(time,t);
+            ans=max(t,ans);
             q.pop();
             for(int i=0;i<4;i++)
             {
-                int nrow=r+drow[i];
-                int ncol=c+dcol[i];
+                int nrow=row+drow[i];
+                int ncol=col+dcol[i];
                 if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]==0 && grid[nrow][ncol]==1)
                 {
+                    vis[nrow][ncol]=1;
                     q.push({{nrow,ncol},t+1});
-                    vis[nrow][ncol]=2;
                     cnt++;
                 }
             }
         }
-        return cnt==countFresh?time:-1;
+        return cnt==countfresh?ans:-1;
     }
 };
