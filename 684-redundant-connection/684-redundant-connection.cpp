4fa257c71cd903ebb989b89b031c7c36288d7ba6
Nimit Jain <55830302+Nimit5@users.noreply.github.com>
2022-09-06 11:35:31 +0530

    Time: 7 ms (89.36%), Space: 9.1 MB (34.57%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -1,35 +1,85 @@
 class Solution {
+
 public:
+
+   int find(int node,vector<int>&parent){
+
+        if(node!=parent[node]){
+
+            parent[node] = find(parent[node],parent);
+
+        }
+
+       return parent[node];
+
+    }
+
+    bool unionNode(int first,int second,vector<int>&rank,vector<int>&parent){
+
+        int p1 = find(first,parent);
+
+        int p2 = find(second,parent);
+
+        // already they are connected
+
+        if(p1 == p2){
+
+            return false;
+
+        }
+
+        // union by rank
+
+        if(rank[p1]>rank[p2]){
+
+            parent[p2] = p1;
+
+            rank[p1]+=rank[p2];
+
+        }
+
+        else{
+
+            parent[p1] = p2;
+
+            rank[p2] += rank[p1];
+
+        }
+
+        return true;
+
+    }
+
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
 
-    vector<int> p(edges.size() + 1, -1);
-    vector<int> rank(edges.size() + 1, 1);
-    
-    function<int(int)> find;
-    
-    find = [&p, &find] (int x) {
-        if (p[x] == -1) return x;
-        return p[x] = find(p[x]);
-    };
-    
-    auto dunion = [&p, &rank](int px, int py){
-        if (rank[px] < rank[py]){
-            p[px] = py;
-        } else if (rank[px] > rank[py]) {
-            p[py] = px;
-        } else {
-            p[px] = py;
-            ++rank[py];
+        int n = edges.size();
+
+        vector<int>parent(n+1),rank(n+1);
+
+        for(int i=1;i<=n;i++){
+
+            parent[i] = i;
+
+            rank[i] = 1;
+
         }
-    };
-    for (auto &edge : edges){
-        int px = find(edge[0]);
-        int py = find(edge[1]);
-        if (px == py)
-            return edge;
-        dunion(px, py);
+
+        vector<int>ans;
+
+        for(auto it:edges){
+
+            if(!unionNode(it[0],it[1],rank,parent)){
+
+                return {it[0],it[1]};
+
+            }
+
+        }
+
+        return {};
+
+       
+
     }
-    
-    return vector<int>();
-}
+
 };
