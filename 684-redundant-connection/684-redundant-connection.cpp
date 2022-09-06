e8598e8bc4e2a692b059d6e6579985f5ed553516
Nimit Jain <55830302+Nimit5@users.noreply.github.com>
2022-09-06 11:39:00 +0530

    Time: 22 ms (20.70%), Space: 9 MB (45.40%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -1,85 +1,46 @@
 class Solution {
-
 public:
-
-   int find(int node,vector<int>&parent){
-
+    int find(int node, vector<int>&parent)
+    {
         if(node!=parent[node]){
-
-            parent[node] = find(parent[node],parent);
-
+            parent[node]=find(parent[node],parent);
         }
-
-       return parent[node];
-
+        return parent[node];
     }
-
-    bool unionNode(int first,int second,vector<int>&rank,vector<int>&parent){
-
-        int p1 = find(first,parent);
-
-        int p2 = find(second,parent);
-
-        // already they are connected
-
-        if(p1 == p2){
-
+    bool union_edge(int u, int v, vector<int>&parent, vector<int>&rank)
+    {
+        int par_u=find(u,parent);
+        int par_v=find(v,parent);
+        if(par_u == par_v){
             return false;
-
         }
-
-        // union by rank
-
-        if(rank[p1]>rank[p2]){
-
-            parent[p2] = p1;
-
-            rank[p1]+=rank[p2];
-
+        
+        if(rank[par_u]>rank[par_v])
+        {
+            parent[par_v]=par_u;
+            rank[par_u]+=rank[par_v];
         }
-
         else{
-
-            parent[p1] = p2;
-
-            rank[p2] += rank[p1];
-
+            parent[par_u]=par_v;
+            rank[par_v]+=rank[par_u];
         }
-
         return true;
-
     }
-
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-
-        int n = edges.size();
-
-        vector<int>parent(n+1),rank(n+1);
-
-        for(int i=1;i<=n;i++){
-
-            parent[i] = i;
-
+        int n=edges.size();
+        vector<int> rank(n+1),parent(n+1);
+        for(int i=1;i<=n;i++)
+        {
+            parent[i]=i;
             rank[i] = 1;
-
         }
-
-        vector<int>ans;
-
-        for(auto it:edges){
-
-            if(!unionNode(it[0],it[1],rank,parent)){
-
-                return {it[0],it[1]};
-
+        for(auto edge:edges)
+        {
+            if(!union_edge(edge[0],edge[1],parent,rank))
+            {
+                return edge;
             }
-
         }
-
         return {};
-
-       
-
     }
-
 };
