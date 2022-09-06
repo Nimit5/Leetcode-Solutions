dd2c84856486b83c70c58c22e3cfb2aba02add47
Nimit Jain <55830302+Nimit5@users.noreply.github.com>
2022-09-06 15:05:45 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (64.29%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.cpp
+++ 814-binary-tree-pruning/814-binary-tree-pruning.cpp
@@ -11,22 +11,11 @@
  */
 class Solution {
 public:
-    bool traverse(TreeNode* root)
-    {
-        if(!root)
-            return false;
-        bool left=traverse(root->left);
-        bool right=traverse(root->right);
-        if(left==false)
-            root->left=NULL;
-        if(right==false)
-            root->right=NULL;
-        return root->val==1 | left | right;
-    }
     TreeNode* pruneTree(TreeNode* root) {
-        bool t=traverse(root);
-        if(!t)
-            return NULL;
+        if (!root) return NULL;
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
+        if (!root->left && !root->right && root->val == 0) return NULL;
         return root;
     }
 };
