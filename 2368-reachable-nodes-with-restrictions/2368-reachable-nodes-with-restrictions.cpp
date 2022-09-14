eb58119e861ceb5fcb8e26ce43f05394ceda3b39
Nimit Jain <55830302+Nimit5@users.noreply.github.com>
2022-09-15 01:04:20 +0530

    Time: 1434 ms (8.45%), Space: 181.8 MB (44.61%) - LeetHub

--- 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
-    void dfs(int node, vector<int>&vis, vector<int>adj[], int &count, unordered_map<int,int>&mp)
+    void dfs(int node, vector<int>&vis, vector<int>adj[], int &count)
     {
         vis[node]=1;
         count++;
         for(auto it:adj[node])
-        {
-            if(!vis[it] and mp.find(it)==mp.end())
-            {
-                dfs(it,vis,adj,count,mp);
-            }
-        }
+            if(!vis[it])
+                dfs(it,vis,adj,count);
     }
     int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
         vector<int> adj[n];
@@ -19,13 +15,12 @@ public:
             adj[edge[0]].push_back(edge[1]);
             adj[edge[1]].push_back(edge[0]);
         }
-        unordered_map<int,int>mp;
-        for(auto it : restricted)
-            mp[it]=1;
-        
         vector<int>vis(n,0);
+        for(auto it : restricted)
+            vis[it]=1;
+        
         int count=0;
-        dfs(0,vis,adj,count,mp);
+        dfs(0,vis,adj,count);
         return count;
     }
 };
