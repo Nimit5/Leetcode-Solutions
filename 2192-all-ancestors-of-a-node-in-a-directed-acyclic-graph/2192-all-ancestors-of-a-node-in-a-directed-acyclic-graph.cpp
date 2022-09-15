0b768076bccd0a023324c435efa5273b748456c1
Nimit Jain <55830302+Nimit5@users.noreply.github.com>
2022-09-16 00:11:04 +0530

    Time: 190 ms (93.71%), Space: 68.8 MB (70.50%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> &graph,int i,int j,vector<vector<int>> &ans){
-        for(auto &x:graph[j]){
-            if(ans[x].empty() || ans[x].back()!=i){
-                ans[x].push_back(i);
-                dfs(graph,i,x,ans);
+    void dfs(int node, vector<vector<int>> &adj, vector<int> &curr, vector<bool> &vis)
+    {
+        vis[node] = true;
+        
+        for(int nbr : adj[node])
+        {
+            if(!vis[nbr])
+            {
+                curr.push_back(nbr);
+                
+                dfs(nbr, adj, curr, vis);
             }
         }
     }
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> ans(n),graph(n);
-        for(auto &v:edges){
-            graph[v[0]].push_back(v[1]);
+        vector<vector<int>> adj(n);
+        for(vector<int> &edge: edges)
+        {
+            adj[edge[1]].push_back(edge[0]);
         }
-        for(int i=0;i<n;i++){
-            dfs(graph,i,i,ans);
+        
+        vector<vector<int>> answer(n);
+        
+        for(int i=0; i<n; ++i)
+        {
+            vector<bool> vis(n, false);
+            
+            vector<int> curr;
+            
+            dfs(i, adj, curr, vis);
+            
+            sort(curr.begin(), curr.end());
+            
+            answer[i] = curr;
         }
-        return ans;
+        return answer;
     }
 };
