d3dd7b3ecba16a962b7996bc5fbff898595d2ebd
Nimit Jain <55830302+Nimit5@users.noreply.github.com>
2022-09-19 00:04:30 +0530

    Time: 326 ms (8.65%), Space: 45.1 MB (31.03%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -9,7 +9,8 @@ public:
             else 
                 return 1e9;
         }
-        if(dp[i][x]!=-1) return dp[i][x];
+        if(dp[i][x]!=-1) 
+            return dp[i][x];
         int nottake=solve(i-1,x,num,dp);
         int take=INT_MAX;
         if(num[i]<=x)
@@ -19,8 +20,23 @@ public:
     }
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        int ans=solve(n-1,amount,coins,dp); 
-        return ans!=1e9?ans:-1;
+        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        
+        for(int x=0;x<=amount;x++)
+            dp[0][x]=x%coins[0]==0?x/coins[0]:1e9;
+        
+        
+        for(int i=1;i<n;i++)
+        {
+            for(int x=0;x<=amount;x++)
+            {
+                int nottake=dp[i-1][x];
+                int take=INT_MAX;
+                if(coins[i]<=x)
+                    take=1+dp[i][x-coins[i]];
+                dp[i][x]=min(nottake,take);
+            }
+        } 
+        return dp[n-1][amount]!=1e9?dp[n-1][amount]:-1;
     }
 };
