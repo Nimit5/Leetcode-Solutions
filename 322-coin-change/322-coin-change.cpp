e2220497a1e30a2032850557d88f3e6ef550a16a
Nimit Jain <55830302+Nimit5@users.noreply.github.com>
2022-09-19 00:10:31 +0530

    Time: 220 ms (22.40%), Space: 40.9 MB (31.77%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,42 +1,15 @@
 class Solution {
 public:
-    int solve(int i, int x, vector<int> &num,vector<vector<int>>& dp)
-    {
-        if(i==0)
-        {
-            if(x%num[i]==0)
-                return x/num[i];
-            else 
-                return 1e9;
-        }
-        if(dp[i][x]!=-1) 
-            return dp[i][x];
-        int nottake=solve(i-1,x,num,dp);
-        int take=INT_MAX;
-        if(num[i]<=x)
-            take=1+solve(i,x-num[i],num,dp);
-        return dp[i][x]=min(nottake,take);
-
-    }
     int coinChange(vector<int>& coins, int amount) {
-        int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,0));
-        
+        vector<int>prev(amount+1,0);
         for(int x=0;x<=amount;x++)
-            dp[0][x]=x%coins[0]==0?x/coins[0]:1e9;
-        
-        
-        for(int i=1;i<n;i++)
-        {
+            prev[x]=x%coins[0]==0?x/coins[0]:1e9;
+        for(int i=1;i<coins.size();i++){
+            vector<int>curr(amount+1,0);
             for(int x=0;x<=amount;x++)
-            {
-                int nottake=dp[i-1][x];
-                int take=INT_MAX;
-                if(coins[i]<=x)
-                    take=1+dp[i][x-coins[i]];
-                dp[i][x]=min(nottake,take);
-            }
+                curr[x]=min(prev[x],coins[i]<=x?1+curr[x-coins[i]]:INT_MAX);
+            prev=curr;
         } 
-        return dp[n-1][amount]!=1e9?dp[n-1][amount]:-1;
+        return prev[amount]!=1e9?prev[amount]:-1;
     }
 };
