d54990057aa453c3c8ea9d7618accacc0d633b43
Nimit Jain <55830302+Nimit5@users.noreply.github.com>
2022-09-21 12:46:11 +0530

    Time: 300 ms (7.54%), Space: 49.8 MB (20.44%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
@@ -1,63 +1,19 @@
 class Solution {
 public:
     vector<int> sumEvenAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
-        vector<int>res;
-        set<int>odd,even;
-        int ans=0;
+        int sum=0;
         for(int i=0;i<nums.size();i++)
-        {
-            if(abs(nums[i])%2==1)
-            {
-                odd.insert(i);
-            }
-            else
-            {
-                ans+=nums[i];
-                even.insert(i);
-            }
-        }
-        cout<<ans<<"\n";
-        cout<<odd.size()<<"\n";
-        cout<<even.size()<<"\n";
+            if(nums[i]%2==0)
+                sum+=nums[i];
+        vector<int>res;
         for(auto q:queries)
         {
-            if(even.find(q[1])!=even.end())
-            {
-                cout<<"Even for "<<q[0]<<","<<q[1]<<"->";
-                if((q[0])%2==0)
-                {
-                    ans+=q[0];
-                    nums[q[1]]+=q[0];
-                    cout<<"even & ans="<<ans<<"\n";
-                }    
-                else
-                {
-                    ans-=nums[q[1]];
-                    nums[q[1]]+=q[0];
-                    even.erase(q[1]);
-                    odd.insert(q[1]);
-                    cout<<"odd & ans="<<ans<<"\n";
-                }
-            }
-            else
-            {
-                cout<<"Odd for "<<q[0]<<","<<q[1]<<"->";
-                if((q[0])%2==0)
-                {
-                    nums[q[1]]+=q[0];
-                    cout<<"even & ans="<<ans<<"\n";
-                }    
-                else
-                {
-                    nums[q[1]]+=q[0];
-                    ans+=nums[q[1]];
-                    odd.erase(q[1]);
-                    even.insert(q[1]);
-                    cout<<"odd & ans="<<ans<<"\n";
-                }
-            }
-            cout<<ans<<"\n";
-            res.push_back(ans);
+            if (nums[q[1]] % 2 == 0) 
+                sum -= nums[q[1]];
+            nums[q[1]] += q[0];
+            if (nums[q[1]] % 2 == 0) 
+                sum += nums[q[1]];
+            res.push_back(sum);
         }
         return res;   
     }
