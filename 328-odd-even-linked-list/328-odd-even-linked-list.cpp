d7057702e195a81f3ea8dd5192294abdaca29ea3
Nimit Jain <55830302+Nimit5@users.noreply.github.com>
2022-09-28 19:37:43 +0530

    Time: 28 ms (24.38%), Space: 10.6 MB (29.21%) - LeetHub

--- 328-odd-even-linked-list/328-odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/328-odd-even-linked-list.cpp
@@ -11,60 +11,16 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if(!head)return head;
-        ListNode *e_head=NULL,*e_tail=NULL,*o_tail=NULL,*o_head=NULL,*curr=head;
-        vector<int>flag;
-        int i=0;
-        while(curr)
-        {
-            if(i%2==0)
-            {
-                if(e_head==NULL)
-                {
-                    flag.push_back(0);
-                    e_head=curr;
-                    e_tail=curr;
-                }
-                else
-                {
-                    e_tail->next=curr;
-                    e_tail=e_tail->next;
-                }
-            }
-            else
-            {
-                if(o_head==NULL)
-                {
-                    flag.push_back(1);
-                    o_head=curr;
-                    o_tail=curr;
-                }
-                else
-                {
-                    o_tail->next=curr;
-                    o_tail=o_tail->next;
-                }
-            }
-            curr=curr->next;
-            i++;
+        if (head == nullptr) return nullptr;
+        ListNode *odd = head, *even = head->next, *evenHead = even;
+        while (even != nullptr && even->next != nullptr) {
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
         }
-        if(flag[0]==0)
-        {
-            if(e_tail)
-                e_tail->next=o_head;
-            if(o_tail)
-                o_tail->next=NULL;
-            return e_head?e_head:o_head;
-        }
-        else
-        {
-            if(o_tail)
-                o_tail->next=e_head;
-            if(e_tail)
-                e_tail->next=NULL;
-            return o_head?o_head:e_head;
-        }
-        
+        odd->next = evenHead;
+        return head;
     }
 };
 // 1 2 3 4 5
